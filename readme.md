a4d267d528f196b5071209b10c8cb68dd1c30cdd
vzambelli <valentin.zambelli@gmail.com>
2015-04-21 18:43:52 +0200

    added readme

---
+++ readme.md
@@ -0,0 +1,2 @@
+dwelly.com offers a lightweight API to retrieve EU VAT rates based on IP addresses
+Go on www.dwelly.com for more info
--- routes/views.js
+++ routes/views.js
@@ -1,22 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var request = require('superagent');
 var bodyParser = require('body-parser');
 
 
 router.get('/',function(req,res){
-   request
-   .get('http://localhost:3000/api/vat/54.154.87.28')
-   .end(function(err, r){
-     if(err){
-       console.log(err);
-
-    } else {
-      res.render('index',{
-        result:r.rates
-      });
-    }
-     });
+  res.render('index');
 });
 
 module.exports = router;
