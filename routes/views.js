7e1d5d40b0ae87f6b752865a27732998c3e8be6f
vzambelli <valentin.zambelli@gmail.com>
2015-04-21 18:27:50 +0200

    changed index files

--- routes/views.js
+++ routes/views.js
@@ -6,14 +6,14 @@ var bodyParser = require('body-parser');
 
 router.get('/',function(req,res){
    request
-   .get('http://dwelly.me/api/vat/54.154.87.28')
+   .get('http://localhost:3000/api/vat/54.154.87.28')
    .end(function(err, r){
      if(err){
        console.log(err);
 
     } else {
       res.render('index',{
-        res:r
+        result:r.rates
       });
     }
      });
